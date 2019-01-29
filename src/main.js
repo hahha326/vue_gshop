9cfeb42b6be311ba013d120dbec0e899eeac82fc
gshop <gshop@example.com>
2019-01-29 16:29:25 +0800

    init gshop

---
+++ src/main.js
@@ -0,0 +1,11 @@
+/* 入口js */
+/* eslint-disable */
+import Vue from "vue"
+import App from "./App.vue"
+import router from "./router"
+
+new Vue({
+  el: "#app",
+  render: h => h(App),
+  router
+})
